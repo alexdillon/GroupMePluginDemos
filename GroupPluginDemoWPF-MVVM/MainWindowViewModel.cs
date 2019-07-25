7ce527affbf8fb8a1cccf9c34724cc390c6269e7
Alex Dillon <alexdillon@knology.net>
2019-07-24 23:42:38 -0500

    Add the WPF-MMVM demo for GroupPlugins

---
+++ GroupPluginDemoWPF-MVVM/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GroupMeClientApi.Models;
+
+namespace GroupPluginDemoWPF_MVVM
+{
+    class MainWindowViewModel : GalaSoft.MvvmLight.ViewModelBase
+    {
+        private string someGeneratedString;
+
+        public MainWindowViewModel(IMessageContainer messageContainer)
+        {
+            this.MessageContainer = messageContainer;
+
+            this.SomeGeneratedString = System.DateTime.Now.ToLongTimeString();
+        }
+
+        public string Name => this.MessageContainer.Name;
+
+        public string SomeGeneratedString
+        {
+            get { return this.someGeneratedString; }
+            private set { this.Set(() => this.SomeGeneratedString, ref this.someGeneratedString, value); }
+        }
+
+        private IMessageContainer MessageContainer { get; }
+    }
+}
---
+++ GroupPluginDemoWPF-MVVM/MyPlugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroupMeClientApi.Models;
+
+namespace GroupPluginDemoWPF_MVVM
+{
+    public class MyPlugin : GroupMeClientPlugin.IPluginBase, GroupMeClientPlugin.GroupChat.IGroupChatPlugin
+    {
+        public string PluginName => "Group Stats Demo WPF with MVVM";
+
+        public Task Activated(IMessageContainer groupOrChat)
+        {
+            MainWindow mainWindow = new MainWindow(); // application entry point
+            MainWindowViewModel vm = new MainWindowViewModel(groupOrChat); // manually bind the ViewModel since App.xaml is missing in libraries
+            mainWindow.DataContext = vm;
+
+            System.Windows.Application.Current.Dispatcher.Invoke(() =>
+            {
+                mainWindow.ShowDialog();    
+            });
+
+            // Do any cleanup needed
+
+            return Task.CompletedTask;
+        }
+    }
+}
