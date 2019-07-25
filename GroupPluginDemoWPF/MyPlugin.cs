c651eed4c4cc1e155514464c7bc3cc379f3f2ddd
Alex Dillon <alexdillon@knology.net>
2019-07-24 23:42:52 -0500

    Add the regular WPF Demo for GroupPlugins

---
+++ GroupPluginDemoWPF/MyPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroupMeClientApi.Models;
+
+namespace GroupPluginDemoWPF
+{
+    public class MyPlugin : GroupMeClientPlugin.IPluginBase, GroupMeClientPlugin.GroupChat.IGroupChatPlugin
+    {
+        public string PluginName => "Group Stats Demo WPF";
+
+        public Task Activated(IMessageContainer groupOrChat)
+        {
+            MainWindow mainWindow = new MainWindow(groupOrChat);
+
+            System.Windows.Application.Current.Dispatcher.Invoke(() =>
+            {
+                mainWindow.ShowDialog();    
+            });
+
+            // Do any cleanup needed
+
+            return Task.CompletedTask;
+        }
+    }
+}
