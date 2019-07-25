f774452ebca3cb3bcdaac0c5bb6d4e2401b4db14
Alex Dillon <alexdillon@knology.net>
2019-07-24 23:43:18 -0500

    Add a NetStandard Class Library demo for composition effects.

---
+++ EffectDemo/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using GroupMeClientPlugin.MessageCompose;
+
+namespace EffectDemo
+{
+    public class Class1 : GroupMeClientPlugin.IPluginBase, GroupMeClientPlugin.MessageCompose.IMessageComposePlugin
+    {
+        public string EffectPluginName => throw new NotImplementedException();
+
+        public Task<MessageSuggestions> ProvideOptions(string typedMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
