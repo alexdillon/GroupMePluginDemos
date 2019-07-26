c8e1ec40f6c66193e3796e54882348bf06abe64e
Alex Dillon <alexdillon@knology.net>
2019-07-26 00:05:50 -0500

    Added the Common Effects Plugin

---
+++ CommonEffectsPlugin/CommonMessagesPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using GroupMeClientPlugin.MessageCompose;
+
+namespace CommonMessagesPlugin
+{
+    public class CommonMessagesPlugin : GroupMeClientPlugin.IPluginBase, IMessageComposePlugin
+    {
+        public string EffectPluginName => "Common Messages";
+
+        public Task<MessageSuggestions> ProvideOptions(string typedMessage)
+        {
+            var result = new MessageSuggestions();
+            result.TextOptions.Add(@"¯\_(ツ)_/¯");
+
+            return Task.FromResult<MessageSuggestions>(result);
+        }
+    }
+}
